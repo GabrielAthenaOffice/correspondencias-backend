f32c38c9de768f8619cbc2af5a148634eed9c3a0
GabrielAthenaOffice <gabrielathenaoffice@gmail.com>
2025-10-08 12:34:14 -0300

    espero ser a ultima mudança

--- src/main/java/com/recepcao/correspondencia/config/CorsConfig.java
+++ src/main/java/com/recepcao/correspondencia/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174") // ajustar conforme necessário
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "https://front-correspondencias-athena.vercel.app") // ajustar conforme necessário
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
